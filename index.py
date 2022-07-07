1c64a82d517df62e624d114258d19cf28f8e32b3
Debanjan Be <debanjanb01234@gmail.com>
2022-07-07 19:14:22 +0530

    time complexity of different programs in python

--- index.py
+++ index.py
@@ -1,17 +1,14 @@
-# Insertion Sort in Python Implementation
-def insertion_sort(A):
-    for j in range(1, len(A)):
-        Key = A[j]
-
-        # insert A[j] into sorted list, A[0, ..., j-1]
-        i = j-1
-        while i >= 0 and A[i] > Key:
-            A[i+1] = A[i]
-            i = i-1
-
-        A[i+1] = Key
-
-    print(A)
+import math
 
 
-insertion_sort([6, 5, 3, 1, 8, 7, 2, 4])        # [1, 2, 3, 4, 5, 6, 7, 8]
+def A(n):
+    count = 0
+    for i in range(1, n+1):
+        for j in range(1, (i ^ 2)+1):
+            for k in range(1, math.floor(n/2)+1):
+                print('Deb')
+                count += 1
+    print(count)
+
+
+A(4)
