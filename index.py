ed52a1cb2e622bffa157f34c48b9b1ebb6c2c348
Debanjan Be <debanjanb01234@gmail.com>
2022-07-08 17:49:52 +0530

    time complexity for loops in python

--- index.py
+++ index.py
@@ -1,14 +1,8 @@
-import math
-
-
 def A(n):
-    count = 0
-    for i in range(1, n+1):
-        for j in range(1, (i ^ 2)+1):
-            for k in range(1, math.floor(n/2)+1):
-                print('Deb')
-                count += 1
-    print(count)
+    i = 1
+    while i <= n:
+        print('Deb {}'.format(i))
+        i = i*2
 
 
-A(4)
+A(5)
